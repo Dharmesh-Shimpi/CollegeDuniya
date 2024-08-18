028a08142bdad3a538feca341840683be3c6815b
Dharmesh Shimpi <dharmesh.shimpi@gmail.com>
2024-08-18 11:51:38 +0530

    ui update

--- app/Main/Ranking/Ranking.js
+++ app/Main/Ranking/Ranking.js
@@ -13,21 +13,22 @@ export default function Ranking({ ranking }) {
 		<div>
 			{ranking.length > 0 && (
 				<div>
-					<div className="font-bold">{ranking[0].overall_rank}</div>
-					<div className="text-sm">{ranking[0].media_source}</div>
+					<div className="font-bold p-1">{ranking[0].overall_rank}</div>
+					<div className="text-sm p-1">{ranking[0].media_source}</div>
 				</div>
 			)}
 
 			<div
 				onClick={handleClick}
 				className="cursor-pointer">
-				<p className="text-green-500">Other...</p>
+				<p className="text-green-500">Show All</p>
 				{display && (
-					<div>
+					<div className="fixed top-1/3 right-1/5 rounded-2xl bg-white p-5 shadow-2xl flex flex-col items-end">
+						<p onClick={handleClick}>X</p>
 						{ranking.map((query, index) => (
 							<div key={index}>
-								<div className="font-bold">{query.overall_rank}</div>
-								<div className="text-sm">{query.media_source}</div>
+								<div className="font-bold p-1">{query.overall_rank}</div>
+								<div className="text-sm p-1">{query.media_source}</div>
 							</div>
 						))}
 					</div>
